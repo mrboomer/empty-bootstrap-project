53c61b16c01fe6b28c6c67edbe6be776c582f37b
Daniel Escobedo <daniel@adquadrant.com>
2015-05-06 11:33:09 -0700

    Fixed Bootstrap Style Output Path

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -71,7 +71,7 @@ gulp.task('bootstrapStyle', function() {
     .pipe( autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4') )
     .pipe( minify({ keepSpecialComments: 0 }) )
     .pipe( rename({ suffix: '.min'	}) )
-    .pipe( gulp.dest(paths.styles.dest) );
+    .pipe( gulp.dest(paths.styles.dest + 'vendor/') );
 });
 
 gulp.task('bootstrapScript', function() {
